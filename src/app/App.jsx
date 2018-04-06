1970810182144157a6c7b646ed6e4533ca03ba3c
Brandon Kaufhold <brandonkaufhold@gmail.com>
2018-04-05 22:15:22 -0400

    Created my personal barebones webpack config

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return(
+    <div>
+      <h1>Hey there</h1>
+      <h2>Hey efefefef </h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  module: {
+    rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "fast-sass-loader",
+          options: {
+            includePaths: ["./styles/"]
+          }
+        }]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
